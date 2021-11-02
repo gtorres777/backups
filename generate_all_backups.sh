49d276823631b59ee681a029961283fd472ae16c
gtorres777 <tuxknrp@gmail.com>
2021-11-02 15:45:28 -0500

    validating not to upload test deploys

--- generate_all_backups.sh
+++ generate_all_backups.sh
@@ -8,16 +8,23 @@ while read deploy; \
    echo "$deploy "
    echo "#############"
 
-   # Get new pod
-   kubectl get pods -n odoo -o=name --field-selector=status.phase=Running | grep $deploy | sed "s/^.\{4\}//" |\
-   while read pod; \
-    do
-      echo "* $pod"
-      echo "- Copyng executing script..."
-      kubectl cp ./backups/generate_backups.py odoo/$pod:/opt/odoo_dir/odoo
-      kubectl cp ./backups/odoo-backup.sh odoo/$pod:/opt/odoo_dir/odoo
-      echo "- Executing script..."
-      kubectl exec -n odoo -t $pod -- bash -c "cd /opt/odoo_dir/odoo && python generate_backups.py localhost:8069 $deploy"
-   done
+
+   if [[ $deploy == *"test"* ]]; 
+   then
+       echo "Test deploy will not be uploaded"
+   else
+       # Get new pod
+       kubectl get pods -n odoo -o=name --field-selector=status.phase=Running | grep $deploy | sed "s/^.\{4\}//" |\
+       while read pod; \
+        do
+          echo "* $pod"
+          echo "- Copyng executing script..."
+          kubectl cp ./backups/generate_backups.py odoo/$pod:/opt/odoo_dir/odoo
+          kubectl cp ./backups/odoo-backup.sh odoo/$pod:/opt/odoo_dir/odoo
+          echo "- Executing script..."
+          kubectl exec -n odoo -t $pod -- bash -c "cd /opt/odoo_dir/odoo && python generate_backups.py localhost:8069 $deploy"
+       done
+   fi
+
 done	
 
